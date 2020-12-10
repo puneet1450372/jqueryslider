9a1931fdbf8d885304b4cccec4738e0c38b01268
puneet1450372 <74813717+puneet1450372@users.noreply.github.com>
2020-12-10 14:05:17 +0530

    Add files via upload

---
+++ style1.js
@@ -0,0 +1,59 @@
+var s;
+var x;
+function page(count){
+    $('figure').css('left', -count + "%");
+   var z=count/100;
+    $('span').removeClass('active');
+    $('span:nth-child('+(z+1)+')').addClass("active");
+    
+   } 
+
+var y = $('img').length;
+        $(document).ready(function () {
+            
+            
+         var x = 0;
+            $('.pre').click(function () {
+
+                if (x >= 100) {
+                    x = (x - 100);
+                }
+                else {
+                    x = 400;
+                }
+                $('figure').css('left', -x + "%");
+                z=x/100;
+                $('span').removeClass('active');
+                $('span:nth-child('+(z+1)+')').addClass("active");
+            });
+
+            $('.next').click(function () {
+                if (x <= 300) {
+                    x = x + 100;
+                }
+                else {
+                    x = 0;
+                }
+                
+                $('figure').css('left', -x + "%");
+                z=x/100;
+                $('span').removeClass('active');
+                $('span:nth-child('+(z+1)+')').addClass("active");
+                });
+            function start(){
+                    s=setInterval(function(){
+                   x= (x<=300)?(x+100):0;
+                var   z=x/100;
+                   $('span').removeClass('active');
+                   $('figure').css('left', -x+'%');
+                   $('span:nth-child('+(z+1)+')').addClass("active");
+                   
+                   },3000);
+                }
+                   
+            function stop(){
+                   clearInterval(s);
+                }
+             $('.next,.pre,figure').on({mouseenter:stop,mouseleave:start});  
+});
+
